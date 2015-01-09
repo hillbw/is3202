7d45865805b82f4f6bdb64c99c96835e9fafaed9
Bruce Hill <bruce.w.hill@gmail.com>
2015-01-09 09:44:01 -0800

    Modified PHP code for Connection to match new database.

--- Lab Assignments/wk6-7/cosmo_shop/Connections/connCosmo.php
+++ Lab Assignments/wk6-7/cosmo_shop/Connections/connCosmo.php
@@ -3,8 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_connCosmo = "localhost";
-$database_connCosmo = "cosmofarmer";
-$username_connCosmo = "cosmo";
-$password_connCosmo = "cosmo";
+$database_connCosmo = "bhill";
+$username_connCosmo = "bhill";
+$password_connCosmo = "bhill";
 $connCosmo = mysql_pconnect($hostname_connCosmo, $username_connCosmo, $password_connCosmo) or trigger_error(mysql_error(),E_USER_ERROR); 
-?>
+?>			
